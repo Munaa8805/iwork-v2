e93d9201bf2981d5f9ef675987d43265446f027b
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-02-04 14:52:59 -0700

    create home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\Job;
 
 class HomeController extends Controller
 {
     //
-    public function index()
+    public function index(): View
     {
-
-        return view('pages.index');
+        $jobs = Job::latest()->limit(5)->get();
+        return view('pages.index',)->with('jobs', $jobs);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,7 +15,19 @@
     <x-slot name="title">
         Home
     </x-slot>
-    
-  
+    <h2 class='text-center text-3xl mb-4 font-bold border border-gray-200 p-4'>
+        Welcome to the Home Page    
+
+    </h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+        @forelse($jobs as $job)
+            <x-job-card :job="$job" />
+        @empty
+            <li>No jobs found</li>
+        @endforelse
+    </div>
+    <a href="{{route('jobs.index')}}" class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
+      <i class="fas fa-arrow-alt-circle-right"> </i> All jobs
+    </a>
 
 </x-layout>
