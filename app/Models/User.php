475682c712468a7c33edaf1f4430d5d4cb22a00d
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-02-09 09:43:29 -0700

    create relationship with user and job

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -50,8 +50,14 @@ class User extends Authenticatable
     }
     //// Related to Job Listings
 
+    // Relate to job listing
     public function jobListings(): HasMany
     {
         return $this->hasMany(JobListing::class);
+    }
+    //// Related to Job Bookmarks
+    public function bookmarkedJobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class, 'job_user_bookmarks')->withTimestamps();
     }
 }
